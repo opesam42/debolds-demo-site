e50f3a6972aa045bbb00ca10c9dd75c85f9b479d
Gbenga Opeyemi <opesam42@gmail.com>
2024-11-06 18:55:33 +0100

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+    content: [
+      "./*.html",
+    ],
+    theme: {
+      extend: {
+        fontFamily: {
+          body: ['Poppins', 'sans-serif'],
+          heading: ['Roboto Condensed', 'serif'],
+          display: ['Inter']
+        },
+      },
+    },
+    plugins: [],
+  }
